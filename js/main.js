6bed0eadbe0682534813db20397f4ba1e1a41e58
AirWav2021 <airwav2015@gmail.com>
2021-08-03 19:09:29 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -3,6 +3,37 @@
 //    document.getElementsByClassName('main').classList.toggle('main--main-bg');
 // }
 
+// const list = document.querySelectorAll('.CatalogMenu li a')
+// list.forEach(item => {
+//    item.addEventListener('click', (e) => {
+//       list.forEach(el => { el.classList.remove('active'); });
+//       item.classList.add('active')
+//    })
+// })
+const menu = document.querySelector(".header__search");
+const button = document.querySelector(".header__search-btn");
+
+// window.addEventListener('click', function (e) {
+//    document.querySelector(".header__search-btn").onclick = function () {
+//       if (!menu.contains(e.target) && !button.contains(e.target)) {
+//          document.querySelector(".header__search").classList.toggle("open");
+//          document.querySelector(".header__search-field").classList.toggle("open");
+//          menu.classList.add('hide');
+//       }
+
+
+//SEARCH
+document.querySelector(".header__search-btn").onclick = function () {
+   document.querySelector(".header__search").classList.toggle("open");
+   document.querySelector(".header__search-field").classList.toggle("open");
+   this.classList.remove("open");
+}
+
+
+
+
+
+// MOBILE MENU   
 document.querySelector(".header__menu").onclick = function () {
    document.querySelector(".header__menu-content").classList.add("open");
 }
